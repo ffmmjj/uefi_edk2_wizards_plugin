c4c11632b9a742c4bdb147cb519a9d6a82be477a
Felipe Martins <ffmmjj@gmail.com>
2015-06-21 16:53:27 -0300

    Estrutura do wizard de criação de projeto melhor organizada.

---
+++ src/org/uefiide/wizards/NewProjectWizard.java
@@ -0,0 +1,26 @@
+package org.uefiide.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NewProjectWizard extends Wizard implements INewWizard {
+
+	public NewProjectWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
