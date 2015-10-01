7ba508d48c4ca6af070a82a8513d2844037e12e8
Felipe Martins <ffmmjj@gmail.com>
2015-10-01 20:29:46 -0300

    A draft of what could be done to add custom build commands to a project(this is definitely not final!)

--- src/org/uefiide/projectmanip/ProjectBuildConfigManager.java
+++ src/org/uefiide/projectmanip/ProjectBuildConfigManager.java
@@ -1,14 +1,27 @@
 package org.uefiide.projectmanip;
 
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.settings.model.ICProjectDescription;
+import org.eclipse.cdt.managedbuilder.core.IBuilder;
+import org.eclipse.cdt.managedbuilder.core.IConfiguration;
+import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.uefiide.structures.Edk2Module;
 
 public class ProjectBuildConfigManager {
-	private IProject project;
 	
-	public ProjectBuildConfigManager(IProject project) {
-		this.project = project;
+	public static void setEDK2BuildCommands(IProject project, Edk2Module edk2Module) {
+		ICProjectDescription projDesc = CCorePlugin.getDefault().getProjectDescription(project, true);
+		IConfiguration cfg = ManagedBuildManager.getConfigurationForDescription(projDesc.getActiveConfiguration());
+		IBuilder builder = cfg.getBuilder();
+		try {
+			builder.setBuildArguments("-p AppPkg/AppPkg.dsc");
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
 	/**
 	 * org.eclipse.cdt.managedbuilder.core.IBuilder shall be used to set the build command which invokes
 	 * "build" from EDK2.
