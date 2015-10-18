cd3521c1237bf1eef47d14dc0c7cc503d1a78e29
Felipe Martins <ffmmjj@gmail.com>
2015-10-18 20:52:23 -0300

    Added menu entry for New EDK2 Module source file - the menu action is not implemented yet

---
+++ src/org/uefiide/handlers/NewEdk2SourceHandler.java
@@ -0,0 +1,53 @@
+package org.uefiide.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class NewEdk2SourceHandler implements IHandler {
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		Shell shell = HandlerUtil.getActiveShell(event);
+	    ISelection sel = HandlerUtil.getActiveMenuSelection(event);
+	    IStructuredSelection selection = (IStructuredSelection) sel;
+	    
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
