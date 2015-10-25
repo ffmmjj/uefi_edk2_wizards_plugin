f1023d9d13fc779bdb0e8521fb5ed830e4676bbc
Felipe Martins <ffmmjj@gmail.com>
2015-10-25 16:23:32 -0300

    Fixed bug which prevented the New EDK2 Source Wizard to be called twice without having to restart Eclipse.

--- src/org/uefiide/handlers/NewEdk2SourceHandler.java
+++ src/org/uefiide/handlers/NewEdk2SourceHandler.java
@@ -17,13 +17,11 @@ public class NewEdk2SourceHandler implements IHandler {
 
 	@Override
 	public void addHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
 
 	}
 
@@ -43,19 +41,16 @@ public class NewEdk2SourceHandler implements IHandler {
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isHandled() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public void removeHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
 
 	}
 
