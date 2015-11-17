1b4f4c0e38a60b2e924f8a65dc7e881313ebb156
Felipe Martins <contact@fmartins.me>
2015-11-16 21:41:05 -0300

    The plugin now automatically assoacite files with the .inf extension to the default text editor instead of the system editor.

---
+++ src/SetDefaultEditorsStartup.java
@@ -0,0 +1,36 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IFileEditorMapping;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.registry.EditorDescriptor;
+import org.eclipse.ui.internal.registry.EditorRegistry;
+import org.eclipse.ui.internal.registry.FileEditorMapping;
+
+public class SetDefaultEditorsStartup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		EditorRegistry editorReg = (EditorRegistry)PlatformUI.getWorkbench().getEditorRegistry();
+		EditorDescriptor editor = (EditorDescriptor)editorReg.findEditor("org.eclipse.ui.DefaultTextEditor");
+		FileEditorMapping mapping = new FileEditorMapping("inf");
+	    mapping.addEditor(editor);
+	    mapping.setDefaultEditor(editor);
+	    
+	    IFileEditorMapping[] mappings = editorReg.getFileEditorMappings();
+	    FileEditorMapping[] newMappings = new FileEditorMapping[mappings.length+1];
+	    for (int i = 0; i < mappings.length; i++) {
+	        newMappings[i] = (FileEditorMapping) mappings[i];
+	    }
+	    newMappings[mappings.length] = mapping;
+	    
+	    Display.getDefault().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				editorReg.setFileEditorMappings(newMappings);
+			}
+		});
+	    
+		//reg.setDefaultEditor("inf", "org.eclipse.ui.DefaultTextEditor");
+	}
+
+}
